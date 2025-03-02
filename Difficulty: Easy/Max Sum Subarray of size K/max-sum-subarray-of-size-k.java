8d5bf5f717cdd80dd74b882ea41c2e9c57e06922
Arfeen Tauhid <157410670+Arfeen-Tauhid@users.noreply.github.com>
2025-03-03 03:03:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int k = Integer.parseInt(br.readLine());
+
+            Solution ob = new Solution();
+            int ans = ob.maximumSumSubarray(arr, k);
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int maximumSumSubarray(int[] arr, int k) {
+        // Code here
+        int m=0;
+        for (int i = 0; i < k; i++) {
+            m += arr[i];
+        }
+        int c=m;
+        for(int i=k;i<arr.length;i++){
+            c += arr[i]-arr[i-k];
+            m=Math.max(c,m);
+        }
+        return m;
+    }
+}
