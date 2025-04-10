43502a335a8f33e80346cd9a571f4c4d3ea3128f
Arfeen Tauhid <157410670+Arfeen-Tauhid@users.noreply.github.com>
2025-04-11 00:13:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim()); // Read number of test cases
+
+        // Loop for each test case
+        while (t-- > 0) {
+            String line = read.readLine().trim(); // Read the array input
+            String[] numsStr = line.split(" ");   // Split the input string by spaces
+            int[] nums =
+                new int[numsStr.length]; // Convert string array to integer array
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int target = Integer.parseInt(read.readLine().trim()); // Read target sum
+
+            Solution ob = new Solution(); // Create an object of Solution class
+            System.out.println(
+                ob.perfectSum(nums, target)); // Call perfectSum and print the result
+            System.out.println("~");          // Call perfectSum and print the result
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    public int perfectSum(int[] arr, int target) {
+        int n = arr.length;
+        int mod = 1000000007;
+
+        int[][] dp = new int[n + 1][target + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int sum = 0; sum <= target; sum++) {
+                if (arr[i - 1] <= sum) {
+                    dp[i][sum] = (dp[i - 1][sum] + dp[i - 1][sum - arr[i - 1]]) % mod;
+                } else {
+                    dp[i][sum] = dp[i - 1][sum];
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
+}
