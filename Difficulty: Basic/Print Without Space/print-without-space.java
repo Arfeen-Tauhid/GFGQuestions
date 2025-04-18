9bacf028b3c473aa2518cd4924c79f37915fb498
Arfeen Tauhid <157410670+Arfeen-Tauhid@users.noreply.github.com>
2025-04-19 00:12:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Without Space/print-without-space.java
@@ -0,0 +1,20 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        // Print in the required format and a new line
+        String c = a+b;
+        System.out.println(c);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
