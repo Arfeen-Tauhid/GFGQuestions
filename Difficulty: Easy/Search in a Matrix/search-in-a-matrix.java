68e6f59827fe2baf19a5fe5d283e74a2a1dcc1f3
Arfeen Tauhid <157410670+Arfeen-Tauhid@users.noreply.github.com>
2025-04-29 23:35:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in a Matrix/search-in-a-matrix.java
+++ Difficulty: Easy/Search in a Matrix/search-in-a-matrix.java
@@ -35,19 +35,15 @@ class GFG {
 // } Driver Code Ends
 
 
-// User function Template for Java
-
 class Solution {
-    // Function to search a given integer in a matrix.
     public boolean searchMatrix(int[][] mat, int x) {
-        // code here
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
-                if(mat[i][j]==x){
-                    return true;
+        for(int i = 0; i < mat.length; i++) { // loop through rows
+            for(int j = 0; j < mat[i].length; j++) { // loop through columns
+                if(mat[i][j] == x) {
+                    return true; // x found
                 }
             }
         }
-        return false;
+        return false; // x not found
     }
 }
