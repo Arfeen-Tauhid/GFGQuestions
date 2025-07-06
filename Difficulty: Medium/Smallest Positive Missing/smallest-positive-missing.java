ff48c5b46053ae7106f2d272c60ffb54d2d1e552
Arfeen Tauhid <157410670+Arfeen-Tauhid@users.noreply.github.com>
2025-07-06 20:36:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            while (arr[i] > 0 && arr[i] <= n && arr[arr[i] - 1] != arr[i]) {
+                // Swap arr[i] with arr[arr[i] - 1]
+                int temp = arr[i];
+                arr[i] = arr[temp - 1];
+                arr[temp - 1] = temp;
+            }
+        }
+
+        // Find the first location where the index doesn't match the value
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If all values are at the correct place
+        return n + 1;
+    }
+}
